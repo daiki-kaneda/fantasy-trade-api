7d23527987242a3caf4674b37e34f3eb424213db
daiki-kaneda <daiki0622developing@gmail.com>
2025-12-10 01:38:19 +0900

    Initial commit

---
+++ src/main/java/com/example/fantasy_trade_api/FantasyTradeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fantasy_trade_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyTradeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyTradeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fantasy_trade_api/FantasyTradeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fantasy_trade_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyTradeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
