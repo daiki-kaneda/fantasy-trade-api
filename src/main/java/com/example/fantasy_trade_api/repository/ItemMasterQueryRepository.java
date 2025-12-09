316703feca19b2af0ef13916994fdc60529dc605
daiki-kaneda <daiki0622developing@gmail.com>
2025-12-10 03:40:40 +0900

    Add ItemMasterQueryRepository

---
+++ src/main/java/com/example/fantasy_trade_api/repository/ItemMasterQueryRepository.java
@@ -0,0 +1,9 @@
+package com.example.fantasy_trade_api.repository;
+
+import org.springframework.data.repository.Repository;
+
+import com.example.fantasy_trade_api.entity.ItemMaster;
+
+public interface ItemMasterQueryRepository extends Repository<ItemMaster,Long>{
+    
+}
