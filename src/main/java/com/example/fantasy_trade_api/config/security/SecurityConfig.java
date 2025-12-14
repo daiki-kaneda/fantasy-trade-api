9b59fb65cc1a8a2a67be61d985fc9f7ee9267387
daiki-kaneda <daiki0622developing@gmail.com>
2025-12-15 00:15:44 +0900

    Add SecurityConfig

---
+++ src/main/java/com/example/fantasy_trade_api/config/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.fantasy_trade_api.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/auth/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .build();
+    }
+}
