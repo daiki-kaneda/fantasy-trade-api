0bfaceb4ca9c403278d188b3c4908341b2ee353d
daiki-kaneda <daiki0622developing@gmail.com>
2025-12-18 23:16:19 +0900

    Add V2__insert_initial_items.sql

---
+++ src/main/resources/db/migration/V2__insert_initial_items.sql
@@ -0,0 +1,58 @@
+-- 初期アイテムデータの挿入
+-- 消耗品 (Potions & Consumables)
+INSERT INTO item_master (name, price) VALUES ('薬草', 10);
+INSERT INTO item_master (name, price) VALUES ('ポーション', 50);
+INSERT INTO item_master (name, price) VALUES ('ハイポーション', 200);
+INSERT INTO item_master (name, price) VALUES ('エクスポーション', 800);
+INSERT INTO item_master (name, price) VALUES ('どく消し', 30);
+INSERT INTO item_master (name, price) VALUES ('気付け薬', 100);
+INSERT INTO item_master (name, price) VALUES ('マジックウォーター', 150);
+INSERT INTO item_master (name, price) VALUES ('エーテル', 500);
+INSERT INTO item_master (name, price) VALUES ('エリクサー', 2000);
+INSERT INTO item_master (name, price) VALUES ('フェニックスの尾', 1000);
+
+-- 武器 (Weapons)
+INSERT INTO item_master (name, price) VALUES ('ひのきの棒', 20);
+INSERT INTO item_master (name, price) VALUES ('錆びた剣', 50);
+INSERT INTO item_master (name, price) VALUES ('銅のつるぎ', 150);
+INSERT INTO item_master (name, price) VALUES ('鉄の剣', 500);
+INSERT INTO item_master (name, price) VALUES ('鋼鉄のつるぎ', 1200);
+INSERT INTO item_master (name, price) VALUES ('銀のレイピア', 2500);
+INSERT INTO item_master (name, price) VALUES ('ミスリルソード', 6000);
+INSERT INTO item_master (name, price) VALUES ('クレイモア', 8500);
+INSERT INTO item_master (name, price) VALUES ('ドラゴンスレイヤー', 25000);
+INSERT INTO item_master (name, price) VALUES ('エクスカリバー', 99999);
+INSERT INTO item_master (name, price) VALUES ('木の杖', 80);
+INSERT INTO item_master (name, price) VALUES ('見習いの魔杖', 400);
+INSERT INTO item_master (name, price) VALUES ('賢者の杖', 4500);
+INSERT INTO item_master (name, price) VALUES ('ショートボウ', 300);
+INSERT INTO item_master (name, price) VALUES ('コンポジットボウ', 1800);
+
+-- 防具 (Armor & Accessories)
+INSERT INTO item_master (name, price) VALUES ('布の服', 30);
+INSERT INTO item_master (name, price) VALUES ('旅人の服', 120);
+INSERT INTO item_master (name, price) VALUES ('皮の鎧', 400);
+INSERT INTO item_master (name, price) VALUES ('鎖かたびら', 1000);
+INSERT INTO item_master (name, price) VALUES ('鉄の鎧', 2500);
+INSERT INTO item_master (name, price) VALUES ('魔法のローブ', 3800);
+INSERT INTO item_master (name, price) VALUES ('プレートメイル', 7000);
+INSERT INTO item_master (name, price) VALUES ('竜鱗の鎧', 18000);
+INSERT INTO item_master (name, price) VALUES ('皮の盾', 150);
+INSERT INTO item_master (name, price) VALUES ('鉄の盾', 800);
+INSERT INTO item_master (name, price) VALUES ('騎士の盾', 3000);
+INSERT INTO item_master (name, price) VALUES ('力の指輪', 5000);
+INSERT INTO item_master (name, price) VALUES ('守りの首飾り', 4500);
+INSERT INTO item_master (name, price) VALUES ('幸運のコイン', 777);
+
+-- 素材・換金アイテム (Materials & Valuables)
+INSERT INTO item_master (name, price) VALUES ('鉄鉱石', 80);
+INSERT INTO item_master (name, price) VALUES ('銀のインゴット', 400);
+INSERT INTO item_master (name, price) VALUES ('金塊', 5000);
+INSERT INTO item_master (name, price) VALUES ('ミスリル原石', 1500);
+INSERT INTO item_master (name, price) VALUES ('竜の鱗', 3000);
+INSERT INTO item_master (name, price) VALUES ('魔力の結晶', 1200);
+INSERT INTO item_master (name, price) VALUES ('古びた硬貨', 10);
+INSERT INTO item_master (name, price) VALUES ('ダイヤモンド', 15000);
+INSERT INTO item_master (name, price) VALUES ('薬草の種', 5);
+INSERT INTO item_master (name, price) VALUES ('キメラの翼', 250);
+INSERT INTO item_master (name, price) VALUES ('聖水', 300);
